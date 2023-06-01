652b0f7d1bc2a05eed527c519e06802293a19e2f
git@git.assembla.com <git@git.assembla.com>
2023-06-01 13:35:34 -0300

    subiendo empezando tests

--- models/models.go
+++ models/models.go
@@ -8,27 +8,6 @@ type Fact struct {
 	Answer   string `json:"answer" gorm:"text;not null;default:null`
 }
 
-type Post struct {
-	gorm.Model
-	Image     string `json:"image" gorm:"text;default:null`
-	Thumbnail string `json:"thumbnail" gorm:"text;not null;default:null`
-	Slug      string `json:"slug" gorm:"text;default:null`
-	Title     string `json:"title" gorm:"text;not null;default:null`
-	Subtitle  string `json:"subtitle" gorm:"text;not null;default:null`
-	Content   string `json:"content" gorm:"text;not null;default:null`
-	Author    int    `json:"author" gorm:"number;not null;default:null`
-	Rate      int    `json:"rate" gorm:"number;not null;default:null`
-}
-
-type Comment struct {
-	gorm.Model
-	PostID  int    `json:"postid" gorm:"number;default:null`
-	Content string `json:"content" gorm:"text;not null;default:null`
-	Author  int    `json:"author" gorm:"number;not null;default:null`
-	Likes   int    `json:"likes" gorm:"number;not null;default:null`
-	Image   string `json:"image" gorm:"text;default:null`
-}
-
 /*
 	class NotificationTypes extends Enum {
 	    public const COMMENT_RECEIVED = 1;
